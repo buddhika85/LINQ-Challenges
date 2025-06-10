38361b51a0e724e916efa951ecbe5fb3839c9d9d
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2025-06-11 08:44:58 +1000

    initial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using LINQ_Challeges.Models;
+using static System.Console;
+
+public class Program
+{
+
+    private static void Main(string[] args)
+    {
+        WriteLine("Hello, LINQ");
+        Challnege_1();
+        
+
+    }
+
+
+    static void Challnege_1()
+    {
+        var numbers = new List<int> { 1, 2, 3, 4, 5 };
+        var query = from num in numbers select num;
+        foreach (var num in numbers) 
+        {
+            WriteLine(num);
+        }
+    }
+}
